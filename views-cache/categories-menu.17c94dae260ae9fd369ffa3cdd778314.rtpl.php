34b75eaa644c220d7198e8d423fde472e453c5c5
Oscar Piriz <owcpirez@gmail.com>
2018-07-18 13:39:49 -0300

    Página categorias

---
+++ views-cache/categories-menu.17c94dae260ae9fd369ffa3cdd778314.rtpl.php
@@ -0,0 +1,1 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/5">Bomboniere</a></li><li><a href="/categories/3">Cama, Mesa e Banho</a></li><li><a href="/categories/2">Eletronicos</a></li><li><a href="/categories/4">Papelaria</a></li>
