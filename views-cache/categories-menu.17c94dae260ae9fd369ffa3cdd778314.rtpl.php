9e8565d68b8685d247022fab68b6f5666bd2bcaa
Oscar Piriz <owcpirez@gmail.com>
2018-08-10 15:19:12 -0300

    Categorias Produtos

--- views-cache/categories-menu.17c94dae260ae9fd369ffa3cdd778314.rtpl.php
+++ views-cache/categories-menu.17c94dae260ae9fd369ffa3cdd778314.rtpl.php
@@ -1,1 +1,1 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/5">Bomboniere</a></li><li><a href="/categories/3">Cama, Mesa e Banho</a></li><li><a href="/categories/2">Eletronicos</a></li><li><a href="/categories/4">Papelaria</a></li>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/5">Bomboniere</a></li><li><a href="/categories/3">Cama, Mesa e Banho</a></li><li><a href="/categories/2">Eletronicos</a></li><li><a href="/categories/4">Papelaria</a></li><li><a href="/categories/6">Telefonia</a></li>
