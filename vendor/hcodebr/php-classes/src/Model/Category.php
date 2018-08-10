669dd87c30dc6038a573f77092e5ba5ae9f2cf50
Oscar Piriz <owcpirez@gmail.com>
2018-08-10 15:43:27 -0300

    Paginação

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -109,7 +109,7 @@ class Category extends Model {
 
 	}
 
-	public function getProductsPage($page = 1, $itemsPerPage = 8)
+	public function getProductsPage($page = 1, $itemsPerPage = 3)
 	{
 
 		$start = ($page - 1) * $itemsPerPage;
